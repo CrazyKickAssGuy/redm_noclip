78f69601946753a4b670971483cfcdf4c2495c45
CrazyKickAssGuy <michaelyakubov98@gmail.com>
2024-10-08 22:12:21 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'adamant'
+game 'rdr3'
+
+author 'Your Name'
+description 'Noclip script for RedM'
+version '1.0.0'
+
+-- Define the client script(s) to be loaded
+client_scripts {
+    'noclip.lua'  -- Your noclip script
+}
